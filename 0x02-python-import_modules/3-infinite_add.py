fc26345ab8c2f425676a3b11c707717e4e270aa9
root <enuzor@gmail.com>
2022-12-01 04:52:35 -0800

    loops

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,18 @@
-#!.usr.bin/python
-if__name__ == "__main__":
-    import sys
-    if len(sys.argv) == 1:
-        print(0)
-    elif len(sys.argv) == 2:
-        print(sys.argv[1])
+#!.usr.bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
     else:
-        sum = 0
-        for i in range(1, len(sys.argv)):
-            a = int(sys.argv[i])
-            sum = sum + a
-            print(sum)
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
